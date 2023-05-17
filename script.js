11d9b7f5d7ea8e44d3f3cd76176f45371f503c55
malken21 <Malken_Marumasa@outlook.jp>
2023-05-17 16:42:39 +0900

    update

--- script.js
+++ script.js
@@ -1,56 +1,35 @@
 
-// 個数などの情報
-let data = {}
+// 購入したデータ (個数など) の情報
+let data = {};
 
 // 個数 追加 関数
-function addItem(id) {
+function addItem(id, value) {
 
     const name = document.getElementById(`name_${id}`).innerText;
     const price = document.getElementById(`price_${id}`).innerText;
-    let count = document.getElementById(`count_${id}`);
-
-    console.log("id: " + id);
-    console.log("name: " + name);
-    console.log("price: " + price);
-    console.log("count: " + count.innerText);
-
+    let count_element = document.getElementById(`count_${id}`);
 
     // 個数が 1024 以上 ならないようにする処理
-    // 上限を 1024 にしたのは 開発者の気分です。 本当は上限無くてもいい
-    if (count.innerText >= 1024) return;
-
-    count.innerText++;
-
-    data[id] = { "name": name, "price": price, "count": count.innerText };
-
-    const money = result();
-    console.log("合計金額: " + money);
-}
-
-// 個数 削除 関数
-function rmvItem(id) {
-
-    const name = document.getElementById(`name_${id}`).innerText;
-    const price = document.getElementById(`price_${id}`).innerText;
-    let count = document.getElementById(`count_${id}`);
-
-    console.log("id: " + id);
-    console.log("name: " + name);
-    console.log("price: " + price);
-    console.log("count: " + count.innerText);
-
     // 個数が 0より 小さくならないようにする処理
-    if (count.innerText < 1) return;
+    // 上限を 1024 にしたのは 開発者の気分です。 本当は上限無くてもいい
+    const count = parseInt(count_element.innerText) + value;
+    if (count >= 1024 || count < 0) return;
 
-    count.innerText--;
+    count_element.innerText = count;
 
-    data[id] = { "name": name, "price": price, "count": count.innerText };
+    // 購入したデータを更新
+    data[id] = { "name": name, "price": price, "count": count };
+    // ログに購入したデータを表示
+    console.log(data);
 
-    const money = result();
-    console.log("合計金額: " + money);
+    // 購入した商品の合計金額が 変数"money" に代入される
+    const money = money_result();
+
+    document.getElementById("result").innerText = money;
 }
 
-function result() {
+// 合計金額算出関数
+function money_result() {
     // 合計金額
     let money = 0;
     Object.keys(data).forEach(key => {
